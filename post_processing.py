f6924dbb5d33877d1cbe93490ba1a958ab6ff6f9
PotatoIsLyfe <163935808+PotatoIsLyfe@users.noreply.github.com>
2024-03-21 17:07:36 -0400

    Update post_processing.py

--- post_processing.py
+++ post_processing.py
@@ -1,1 +1,16 @@
+def post_processing(parray, text):
+  #S plit left and right
+  left = text >> 32       # Gets rid of the 32 bits on the right by shifting right 
+  right = text & 0xFFFFFFFF    # Gets rid of the 32 bits on the left by performing the & operation on the whole 64 bit text with only 32 bits
 
+  # Swap left and right
+  left, right = right, left
+
+  # Final xor with parray
+  left = left ^ parray[16]     
+  right = right ^ parray[17]
+
+  # Recombine the two halfs
+  cipher_text = (left << 32) | right   # Shift the 32 left bits left, to make it 64 bits with lower 32 bits all 0, then or with right bits
+
+  return cipher_text
