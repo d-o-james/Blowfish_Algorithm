914831be138afb6a2785c28029de13a9cef4922d
d-o-james <149330627+d-o-james@users.noreply.github.com>
2024-04-17 09:29:52 -0400

    Update decryption.py

--- decryption.py
+++ decryption.py
@@ -1,2 +1,21 @@
-def decryption(cipher, key):
-  pass
+def Enc(cipher, key):
+
+    cipher = cipher.encode("utf-8") # encodes string to bit string
+
+    subkey_array = subkey_gen(K) # gets subkey array
+
+    cipher_L = cipher[0:len(cipher)//2]
+    cipher_R = cipher[len(cipher)//2:]
+    #splits strings into left and right halfs
+
+    for i in range(16): # blowfish has a 15 round
+        temp = cipher_L ^ subkey_array[15-i].encode("utf-8")
+        temp = pseudorandom(temp) ^ cipher_R
+        # uses temp values to get the new left sides value after being run through F_k
+        
+        cipher_R = cipher_L # copies left side to new right side
+        cipher_L = temp # copies f_k's output to the new left side
+
+    preprocessed_c = cipher_L + cipher_R # combines left and right sides
+
+    return post_processing(subkey_array, preprocessed_c).decode("utf-8") #returns the values after post processing
